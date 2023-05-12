948c66fea680955b640114f4169650582fd60fca
Your Name <you@example.com>
2023-05-13 00:30:05 +0800

    Sever.cpp

--- Server01/server/Server.cpp
+++ Server01/server/Server.cpp
@@ -3,7 +3,7 @@
 #include <string>
 #include <string.h>
 #include <vector>
-
+#include<thread>
 
 #include <ws2tcpip.h>    // 包含inet_pton和inet_ntop等函数
 #include <WinSock2.h>
@@ -13,6 +13,81 @@
 #include "../route/Route.h"
 
 
+
+namespace myServer
+{
+
+
+struct SocketItem  // 与c接口打交道时，不要无脑用封装性，如果这里用class在get和set是及其繁琐的
+{
+    SOCKET socket;
+    struct sockaddr_in servaddr;
+};
+
+
+class Server
+{
+private:
+    SocketItem servSockItem;
+    SocketItem* clientSockArr;
+    struct WSAData wsadata;
+    int clientNum = 4;      // 暂时不能手动设置改值    
+    int clientID = 0;       // 正在等待第号个client连接
+public:
+    Server(const char* IPaddr = "127.0.0.1", unsigned short port = 8888) 
+    {
+        int res = 0;
+        res = WSAStartup(MAKEWORD(2, 2), &wsadata);
+        myLog::log(res, -1, "error: myServer::Server::Server() ==> WSAStartup()失败");
+
+        servSockItem.socket = socket(AF_INET, SOCK_STREAM, 0);
+        myLog::log(static_cast<int>(servSockItem.socket), -1, "error: myServer::Server::Server() ==> socket()失败");
+
+
+        servSockItem.servaddr.sin_family = AF_INET;
+        servSockItem.servaddr.sin_port = htons(port);
+ 
+        inet_pton(AF_INET, IPaddr, &servSockItem.servaddr.sin_addr.s_addr);  /////
+        res = bind(servSockItem.socket, (struct sockaddr*)&servSockItem.servaddr, sizeof(servSockItem.servaddr));
+        myLog::log(res, -1, "error: myServer::Server::Server() ==> bind()失败");
+
+        res = listen(servSockItem.socket, clientNum);
+        myLog::log(res, -1, "error: myServer::Server::Server() ==> listen()失败");
+
+        clientSockArr = new SocketItem[clientNum];
+    }
+    
+    ~Server()
+    {
+        for (int i = 0; i < clientNum; i++) {  // 有问题不完善，要改进？？？？？？？？
+            closesocket(clientSockArr[i].socket);
+        }
+        delete[] clientSockArr;
+
+        closesocket(servSockItem.socket);
+        WSACleanup();
+    }
+    
+    Server(const Server&) = delete;
+    Server& operator= (const Server&) = delete;
+    // 移动操作 ??
+    // const class ??
+
+    SocketItem accept_server()
+    {
+        int addrlen = sizeof(clientSockArr[clientID].servaddr);
+        clientSockArr[clientID].socket = accept(servSockItem.socket, (struct sockaddr*)&clientSockArr[clientID].servaddr, &addrlen); // 与服务端的sockaddr相绑定，返回新的socket
+        myLog::log(static_cast<int>(clientSockArr[clientID].socket), -1, "error: myServer::Server::accept_server()失败");
+        return clientSockArr[clientID++];   // 注意是后置++  未控制clientID < clientNum ？？？？？？？？
+    }
+
+
+};
+
+
+
+
+}
 
 
 void RequestMainFunc(SOCKET* clientsock)
@@ -49,28 +124,87 @@ void RequestMainFunc(SOCKET* clientsock)
 
 
 
+//int main()
+//{
+//	struct WSAData wsadata;
+//	int res = WSAStartup(MAKEWORD(2, 2), &wsadata);
+//	if (res == -1) return -1;
+//
+//	SOCKET servsock = socket(AF_INET, SOCK_STREAM, 0);
+//	if (servsock == -1) return -1;
+//
+//	struct sockaddr_in servaddr;
+//	servaddr.sin_family = AF_INET;
+//	servaddr.sin_port = htons(8888);
+//	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);   // 同 servaddr.sin_addr.S_un.S_addr = htonl(INADDR_ANY);
+//
+//
+//	res = bind(servsock, (struct sockaddr*)&servaddr, sizeof(servaddr));
+//	if (res == -1) return -1;
+//
+//	res = listen(servsock, 4);
+//	if (res == -1) return -1;
+//
+//    
+//    //while (1) {
+//    //    SOCKET clientsock;   // new
+//    //    struct sockaddr_in clientaddr;  // new
+//    //    int addrlen = sizeof(clientaddr);
+//    //    clientsock = accept(servsock, (struct sockaddr*)&clientaddr, &addrlen); // 与服务端的sockaddr相绑定，返回新的socket
+//    //    if (clientsock == -1) return -1;
+//    //    // 创建一个线程
+//    //}
+//    
+//    SOCKET clientsock;
+//    struct sockaddr_in clientaddr;
+//    int addrlen = sizeof(clientaddr);
+//    clientsock = accept(servsock, (struct sockaddr*)&clientaddr, &addrlen); // 与服务端的sockaddr相绑定，返回新的socket
+//    if (clientsock == -1) return -1;
+//
+//
+//    RequestMainFunc(&clientsock);
+//
+//    /*
+//
+//    myRoute::Route route{};
+//
+//    while (1) {   // 为什么顶部的页面栏一直在转圈圈？ 怎么设置顶部的页面栏的图标？
+//
+//        char buf[1024] = { 0 };
+//        int len = 0;
+//
+//        len = recv(clientsock, buf, 1024, 0);
+//        if (len == -1) return 0;
+//        printf("%s\n", buf);  // 输出get请求报
+//
+//
+//        char url[1024] = { 0 };
+//        sscanf(buf, "GET %s HTTP/1.1", url);   // 用正则表达式获得文件路径
+//        printf(" ==> url: %s\n", url);           // 输出url
+//
+//        std::vector<void*> res(10, nullptr);
+//        res[0] = &clientsock;
+//
+//
+//        myRoute::RouteABS* r1 = route.get_routeElement(url);
+//        r1->dealRequest(buf, buf, res);
+//    }
+//    */
+//	// Sleep(1000);    // 要延迟一下，浏览器加载页面要时间不能，直接关闭服务器
+//
+//	closesocket(clientsock);   // 注意释放资源的顺序
+//	closesocket(servsock);
+//	WSACleanup();
+//	return 0;
+//}
+
+
+
+
 int main()
 {
-	struct WSAData wsadata;
-	int res = WSAStartup(MAKEWORD(2, 2), &wsadata);
-	if (res == -1) return -1;
-
-	SOCKET servsock = socket(AF_INET, SOCK_STREAM, 0);
-	if (servsock == -1) return -1;
-
-	struct sockaddr_in servaddr;
-	servaddr.sin_family = AF_INET;
-	servaddr.sin_port = htons(8888);
-	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);   // 同 servaddr.sin_addr.S_un.S_addr = htonl(INADDR_ANY);
-
-
-	res = bind(servsock, (struct sockaddr*)&servaddr, sizeof(servaddr));
-	if (res == -1) return -1;
-
-	res = listen(servsock, 4);
-	if (res == -1) return -1;
-
     
+
     //while (1) {
     //    SOCKET clientsock;   // new
     //    struct sockaddr_in clientaddr;  // new
@@ -79,161 +213,19 @@ int main()
     //    if (clientsock == -1) return -1;
     //    // 创建一个线程
     //}
+
+    //SOCKET clientsock;
+    //struct sockaddr_in clientaddr;
+    //int addrlen = sizeof(clientaddr);
+    //clientsock = accept(servsock, (struct sockaddr*)&clientaddr, &addrlen); // 与服务端的sockaddr相绑定，返回新的socket
+    //if (clientsock == -1) return -1;
+
+
+    myServer::Server server{};  // myServer::Server server("127.10.10.1",11111);
+    myServer::SocketItem clientItem01 = server.accept_server();
+    RequestMainFunc(&clientItem01.socket);
+
     
-    SOCKET clientsock;
-    struct sockaddr_in clientaddr;
-    int addrlen = sizeof(clientaddr);
-    clientsock = accept(servsock, (struct sockaddr*)&clientaddr, &addrlen); // 与服务端的sockaddr相绑定，返回新的socket
-    if (clientsock == -1) return -1;
-
-
-    RequestMainFunc(&clientsock);
-
-    /*
-
-    myRoute::Route route{};
-
-    while (1) {   // 为什么顶部的页面栏一直在转圈圈？ 怎么设置顶部的页面栏的图标？
-
-        char buf[1024] = { 0 };
-        int len = 0;
-
-        len = recv(clientsock, buf, 1024, 0);
-        if (len == -1) return 0;
-        printf("%s\n", buf);  // 输出get请求报
-
-
-        char url[1024] = { 0 };
-        sscanf(buf, "GET %s HTTP/1.1", url);   // 用正则表达式获得文件路径
-        printf(" ==> url: %s\n", url);           // 输出url
-
-        std::vector<void*> res(10, nullptr);
-        res[0] = &clientsock;
-
-
-        myRoute::RouteABS* r1 = route.get_routeElement(url);
-        r1->dealRequest(buf, buf, res);
-    }
-    */
-	// Sleep(1000);    // 要延迟一下，浏览器加载页面要时间不能，直接关闭服务器
-
-	closesocket(clientsock);   // 注意释放资源的顺序
-	closesocket(servsock);
-	WSACleanup();
-	return 0;
-}
-
-
-/*// 参考代码
-
-#include <stdio.h>
-#include <winsock2.h>
-
-#pragma comment(lib, "ws2_32.lib")  // 链接 Winsock2 库
-
-int main()
-{
-    WSADATA wsaData;                    // 存储 Winsock 库初始化信息的结构体
-    SOCKET listenSocket = INVALID_SOCKET;// 监听客户端连接请求的套接字对象
-    SOCKET clientSocket = INVALID_SOCKET;// 与客户端进行数据交换的套接字对象
-    struct addrinfo* result = NULL;     // 待连接主机的地址信息列表
-    struct addrinfo hints;              // 连接的地址信息结构体
-    int ret;
-
-    // 初始化 Winsock 库
-    ret = WSAStartup(MAKEWORD(2, 2), &wsaData);
-    if (ret != 0)
-    {
-        printf("WSAStartup failed with error: %d\n", ret);
-        return 1;
-    }
-
-    // 填充初始地址信息结构体（仅支持 IPv4）
-    ZeroMemory(&hints, sizeof(hints));
-    hints.ai_family = AF_INET;          // IPv4 地址族
-    hints.ai_socktype = SOCK_STREAM;    // 流式套接字
-    hints.ai_protocol = IPPROTO_TCP;    // TCP 协议
-    hints.ai_flags = AI_PASSIVE;        // 被动连接模式
-
-    // 获取待连接的本地 IP 地址和端口号
-    ret = getaddrinfo(NULL, "8080", &hints, &result);
-    if (ret != 0)
-    {
-        printf("getaddrinfo failed with error: %d\n", ret);
-        WSACleanup();
-        return 1;
-    }
-
-    // 创建套接字对象并绑定到本地 IP 地址和端口号上
-    listenSocket = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
-    if (listenSocket == INVALID_SOCKET)
-    {
-        printf("socket failed with error: %ld\n", WSAGetLastError());
-        freeaddrinfo(result);
-        WSACleanup();
-        return 1;
-    }
-    ret = bind(listenSocket, result->ai_addr, (int)result->ai_addrlen);
-    if (ret == SOCKET_ERROR)
-    {
-        printf("bind failed with error: %d\n", WSAGetLastError());
-        freeaddrinfo(result);
-        closesocket(listenSocket);
-        WSACleanup();
-        return 1;
-    }
-    freeaddrinfo(result);
-
-    // 监听本地 IP 地址上指定的端口，等待连接请求
-    if (listen(listenSocket, SOMAXCONN) == SOCKET_ERROR)
-    {
-        printf("listen failed with error: %d\n", WSAGetLastError());
-        closesocket(listenSocket);
-        WSACleanup();
-        return 1;
-    }
-
-    // 循环处理客户端请求
-    while (true)
-    {
-        // 接收客户端连接请求，并创建新的套接字对象用于接收数据
-        clientSocket = accept(listenSocket, NULL, NULL);
-        if (clientSocket == INVALID_SOCKET)
-        {
-            printf("accept failed with error: %d\n", WSAGetLastError());
-            closesocket(listenSocket);
-            WSACleanup();
-            return 1;
-        }
-
-        // 组织 HTTP 响应报文并发送给客户端
-        char responseHeader[] = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n";
-        send(clientSocket, responseHeader, sizeof(responseHeader), 0);
-
-        // 发送指定的 HTML 文件内容（注意要使用二进制方式读取文件）
-        FILE* fp;
-        ret = fopen_s(&fp, "test.html", "rb");
-        if (ret != 0)
-        {
-            printf("open file 'test.html' failed with error: %d\n", ret);
-            continue;
-        }
-        char buffer[1024];
-        int bytesRead;
-        while ((bytesRead = fread(buffer, sizeof(char), sizeof(buffer), fp)) > 0)
-        {
-            send(clientSocket, buffer, bytesRead, 0);
-        }
-        fclose(fp);
-
-        // 关闭与客户端的连接和套接字对象
-        closesocket(clientSocket);
-    }
-
-    // 关闭监听套接字对象并完成 Winsock 库的清理工作
-    closesocket(listenSocket);
-    WSACleanup();
-
     return 0;
 }
-*/
+
