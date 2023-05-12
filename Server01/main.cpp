33920d29bae5dcf69ab14fd7754c2a8648ddb2f5
Your Name <you@example.com>
2023-05-13 01:04:27 +0800

    new

---
+++ Server01/main.cpp
@@ -0,0 +1,57 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <string>
+#include <string.h>
+#include <vector>
+
+#include "route/Route.h"
+#include "server/Server.h"
+
+
+void RequestMainFunc(SOCKET* clientsock)
+{
+    myRoute::Route route{};
+    std::vector<void*> res(10, nullptr);
+    res[0] = clientsock;
+
+    char buf[1024] = { 0 };
+    int len = 0;
+    char url[1024] = { 0 };
+
+    while (1) {   // 为什么顶部的页面栏一直在转圈圈？ 怎么设置顶部的页面栏的图标？
+
+
+
+        len = recv(*clientsock, buf, 1024, 0);
+        myLog::log(len, -1, "error: recv()失败 ==> clientsock:连接以停止");
+        // myLog::log(len, -1, "error: myRoute::route::get_fileAddr()失败 ==> " + "不存在");  // 为什么不行？？？？
+        printf("%s\n", buf);  // 输出get请求报
+
+        sscanf(buf, "GET %s HTTP/1.1", url);   // 用正则表达式获得文件路径
+        printf(" ==> url: %s\n", url);           // 输出url
+
+
+        myRoute::RouteABS* routeElement = route.get_routeElement(url);
+        routeElement->dealRequest(buf, buf, res);
+
+    }
+
+
+    // delete vector element ???????
+}
+
+
+
+int main()
+{
+
+    // 用std::thread实现一个线程池，accept操作也要一个单独的线程控制吗(先看看笔记)。
+
+    myServer::Server server{};  // myServer::Server server("127.10.10.1",11111);
+    myServer::SocketItem clientItem01 = server.accept_server();
+    RequestMainFunc(&clientItem01.socket);
+
+
+    return 0;
+}
---
+++ Server01/server/Server.h
@@ -0,0 +1,107 @@
+
+
+
+#ifndef _SERVER_H_
+#define _SERVER_H_
+
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include <ws2tcpip.h>    // 包含inet_pton和inet_ntop等函数
+#include <WinSock2.h>
+#pragma comment(lib, "ws2_32.lib")
+
+#include "../log/Log.h"
+
+
+
+
+
+
+
+
+namespace myServer
+{
+
+
+struct SocketItem  // 与c接口打交道时，不要无脑用封装性，如果这里用class在get和set是及其繁琐的
+{
+    SOCKET socket;
+    struct sockaddr_in servaddr;
+};
+
+
+class Server
+{
+private:
+    SocketItem servSockItem;
+    SocketItem* clientSockArr;
+    struct WSAData wsadata;
+    int clientNum = 4;      // 暂时不能手动设置改值    
+    int clientID = 0;       // 正在等待第号个client连接
+public:
+    Server(const char* IPaddr = "127.0.0.1", unsigned short port = 8888) 
+    {
+        int res = 0;
+        res = WSAStartup(MAKEWORD(2, 2), &wsadata);
+        myLog::log(res, -1, "error: myServer::Server::Server() ==> WSAStartup()失败");
+
+        servSockItem.socket = socket(AF_INET, SOCK_STREAM, 0);
+        myLog::log(static_cast<int>(servSockItem.socket), -1, "error: myServer::Server::Server() ==> socket()失败");
+
+
+        servSockItem.servaddr.sin_family = AF_INET;
+        servSockItem.servaddr.sin_port = htons(port);
+ 
+        inet_pton(AF_INET, IPaddr, &servSockItem.servaddr.sin_addr.s_addr);  /////
+        res = bind(servSockItem.socket, (struct sockaddr*)&servSockItem.servaddr, sizeof(servSockItem.servaddr));
+        myLog::log(res, -1, "error: myServer::Server::Server() ==> bind()失败");
+
+        res = listen(servSockItem.socket, clientNum);
+        myLog::log(res, -1, "error: myServer::Server::Server() ==> listen()失败");
+
+        clientSockArr = new SocketItem[clientNum];
+    }
+    
+    ~Server()
+    {
+        for (int i = 0; i < clientNum; i++) {  // 有问题不完善，要改进？？？？？？？？
+            closesocket(clientSockArr[i].socket);
+        }
+        delete[] clientSockArr;
+
+        closesocket(servSockItem.socket);
+        WSACleanup();
+    }
+    
+    Server(const Server&) = delete;
+    Server& operator= (const Server&) = delete;
+    // 移动操作 ??
+    // const class ??
+
+    SocketItem accept_server()
+    {
+        int addrlen = sizeof(clientSockArr[clientID].servaddr);
+        clientSockArr[clientID].socket = accept(servSockItem.socket, (struct sockaddr*)&clientSockArr[clientID].servaddr, &addrlen); // 与服务端的sockaddr相绑定，返回新的socket
+        myLog::log(static_cast<int>(clientSockArr[clientID].socket), -1, "error: myServer::Server::accept_server()失败");
+        return clientSockArr[clientID++];   // 注意是后置++  未控制clientID < clientNum ？？？？？？？？
+    }
+
+
+};
+
+
+
+
+}
+
+
+
+
+
+#endif // ！_SERVER_H_
+
+
+
